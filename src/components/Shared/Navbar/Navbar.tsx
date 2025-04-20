97d002435cea34c58c2dc68f19fcfe7209e9a8d8
Sabbir386 <sabbir386.sa@gmail.com>
2025-04-20 00:52:55 -0700

    added

--- src/components/Shared/Navbar/Navbar.tsx
+++ src/components/Shared/Navbar/Navbar.tsx
@@ -1,4 +1,5 @@
-import { Box, Container, Stack, Typography } from "@mui/material";
+import { Box, Button, Container, Stack, Typography } from "@mui/material";
+import Link from "next/link";
 
 
 const Navbar = () => {
@@ -6,9 +7,17 @@ const Navbar = () => {
     <Container>
       <Stack py={2} direction="row" justifyContent="space-between" alignItems="center">
 
-        <Typography variant="h5" component={"h1"} fontWeight={600} >
+        <Typography variant="h4" component={Link} href="/" fontWeight={600} >
           <Box component={"span"} color="primary.main">H</Box>ealtCare 
         </Typography>
+        <Stack  direction="row" gap={4} justifyContent="space-between">
+          <Typography component={Link} href="/consultations">Consultations</Typography>
+          <Typography component={Link} href="/Health Plans">Health Plans</Typography>
+          <Typography component={Link} href="/medicine">Medicine</Typography>
+          <Typography component={Link} href="/diagonostics">Diagonostics</Typography>
+          <Typography component={Link} href="/login">NGOs</Typography>
+        </Stack>
+        <Button component={Link} href="/login">Login</Button>
       </Stack>
     </Container>
   );
