40808579d884505a95927e2e72f965cc18841a60
Sabbir386 <sabbir386.sa@gmail.com>
2025-04-22 07:25:43 -0700

    added why us section

--- src/app/(withCommonLayout)/page.tsx
+++ src/app/(withCommonLayout)/page.tsx
@@ -1,6 +1,7 @@
 import HeroSection from '@/components/Ui/HomePage/HeroSection/HeroSection';
 import Specialist from '@/components/Ui/HomePage/Specialist/Specialist';
 import TopRatedDoctors from '@/components/Ui/HomePage/TopRatedDoctors/TopRatedDoctors';
+import WhyUs from '@/components/Ui/HomePage/WhyUs/WhyUs';
 import { Button } from '@mui/material';
 const HomePage = () => {
   return (
@@ -8,6 +9,8 @@ const HomePage = () => {
       <HeroSection></HeroSection>
      <Specialist></Specialist>
      <TopRatedDoctors></TopRatedDoctors>
+     <WhyUs></WhyUs>
+
     </>
   );
 };
