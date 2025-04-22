4ce4c64198b15382b2fcb1fd8cfe6d954023bb94
Sabbir386 <sabbir386.sa@gmail.com>
2025-04-22 07:10:59 -0700

    added top rated doctores section

--- src/app/(withCommonLayout)/page.tsx
+++ src/app/(withCommonLayout)/page.tsx
@@ -1,11 +1,13 @@
 import HeroSection from '@/components/Ui/HomePage/HeroSection/HeroSection';
 import Specialist from '@/components/Ui/HomePage/Specialist/Specialist';
+import TopRatedDoctors from '@/components/Ui/HomePage/TopRatedDoctors/TopRatedDoctors';
 import { Button } from '@mui/material';
 const HomePage = () => {
   return (
     <>
       <HeroSection></HeroSection>
      <Specialist></Specialist>
+     <TopRatedDoctors></TopRatedDoctors>
     </>
   );
 };
