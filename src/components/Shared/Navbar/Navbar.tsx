df052e5657d1c77a96cee8acbfaea2bf9470f113
Sabbir386 <sabbir386.sa@gmail.com>
2025-04-24 01:31:42 -0700

    added usewr login and logout using localstorage

--- src/components/Shared/Navbar/Navbar.tsx
+++ src/components/Shared/Navbar/Navbar.tsx
@@ -1,26 +1,87 @@
-import { Box, Button, Container, Stack, Typography } from "@mui/material";
-import Link from "next/link";
+'use client';
 
+import useUserInfo from '@/hooks/useUserInfo';
+import { logoutUser } from '@/services/actions/logoutUser';
+import { Box, Button, Container, Stack, Typography } from '@mui/material';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 
 const Navbar = () => {
-  return (
-    <Container>
-      <Stack py={2} direction="row" justifyContent="space-between" alignItems="center">
+   const userInfo = useUserInfo();
+   const router = useRouter();
 
-        <Typography variant="h4" component={Link} href="/" fontWeight={600} >
-          <Box component={"span"} color="primary.main">H</Box>ealtCare 
-        </Typography>
-        <Stack  direction="row" gap={4} justifyContent="space-between">
-          <Typography component={Link} href="/consultations">Consultations</Typography>
-          <Typography component={Link} href="/Health Plans">Health Plans</Typography>
-          <Typography component={Link} href="/medicine">Medicine</Typography>
-          <Typography component={Link} href="/diagonostics">Diagonostics</Typography>
-          <Typography component={Link} href="/login">NGOs</Typography>
-        </Stack>
-        <Button component={Link} href="/login">Login</Button>
-      </Stack>
-    </Container>
-  );
+   const handleLogOut = () => {
+      logoutUser(router);
+   };
+
+   return (
+      <Box
+         sx={{
+            bgcolor: 'primary.main',
+         }}
+      >
+         <Container>
+            <Stack
+               py={2}
+               direction='row'
+               justifyContent='space-between'
+               alignItems='center'
+            >
+               <Typography
+                  variant='h4'
+                  component={Link}
+                  href='/'
+                  fontWeight={600}
+               >
+                  P
+                  <Box component='span' color='#ffffff'>
+                     H
+                  </Box>{' '}
+                  Health Care
+               </Typography>
+
+               <Stack direction='row' justifyContent='space-between' gap={4}>
+                  <Typography
+                     component={Link}
+                     href='/consultation'
+                     color='#ffffff'
+                  >
+                     Consultation
+                  </Typography>
+
+                  <Typography color='#ffffff'>Diagnostics</Typography>
+                  <Typography component={Link} href='/doctors' color='#ffffff'>
+                     Doctors
+                  </Typography>
+
+                  {userInfo?.userId ? (
+                     <Typography
+                        component={Link}
+                        href='/dashboard'
+                        color='#ffffff'
+                     >
+                        Dashboard
+                     </Typography>
+                  ) : null}
+               </Stack>
+
+               {userInfo?.userId ? (
+                  <Button
+                     color='error'
+                     onClick={handleLogOut}
+                     sx={{ boxShadow: 0 }}
+                  >
+                     Logout
+                  </Button>
+               ) : (
+                  <Button component={Link} href='/login'>
+                     Login
+                  </Button>
+               )}
+            </Stack>
+         </Container>
+      </Box>
+   );
 };
 
 export default Navbar;
