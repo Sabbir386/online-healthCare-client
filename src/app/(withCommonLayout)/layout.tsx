05c22a888e7b431ff9f24871f7f5f0df973d6ac4
Sabbir386 <sabbir386.sa@gmail.com>
2025-04-24 00:29:46 -0700

    added user login

--- src/app/(withCommonLayout)/layout.tsx
+++ src/app/(withCommonLayout)/layout.tsx
@@ -1,13 +1,16 @@
 import Footer from '@/components/Shared/Footer/Footer';
 import Navbar from '@/components/Shared/Navbar/Navbar';
 import React from 'react';
+import { Toaster } from 'sonner';
 
 const CommonLayout = ({children}:{children:React.ReactNode}) => {
   return (
     <>
       <Navbar />
       <div className='min-h-screen '>
-      {children}
+      <>
+      <Toaster></Toaster>
+      {children}</>
       </div>
       <Footer />
     </>
