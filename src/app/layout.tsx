0186c90d21b1eb2c49727f9810604ed8a99d182e
Sabbir386 <sabbir386.sa@gmail.com>
2025-04-28 01:18:34 -0700

    added userRole base dashboard

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,31 +1,34 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
-import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter';
 import Providers from '@/lib/Providers/Providers';
-const inter = Inter({ subsets: ['latin'] })
+import { Toaster } from 'sonner';
+
+// const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'HealthCare',
-  description: 'Doctors and patients can connect with each other',
-  viewport: 'width=device-width, initial-scale=1',
-  keywords: ['HealthCare', 'Doctors', 'Patients'],
-}
+   title: 'PH Health Care',
+   description: 'Dashboard',
+};
 
 export default function RootLayout({
-  children,
+   children,
 }: {
-  children: React.ReactNode
+   children: React.ReactNode;
 }) {
-  return (
-    <Providers>
-      <html lang="en">
-    <body className={inter.className}>
-    <AppRouterCacheProvider>
-      {children}
-      </AppRouterCacheProvider>
-      </body>
-  </html>
-  </Providers>
-  )
+   return (
+      <Providers>
+         <html lang='en'>
+            <body>
+               <AppRouterCacheProvider>
+                  <>
+                     <Toaster position='top-center' />
+                     {children}
+                  </>
+               </AppRouterCacheProvider>
+            </body>
+         </html>
+      </Providers>
+   );
 }
